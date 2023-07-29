d2b20f057b3432e3a75b8fd72f822fe0bbe5a1e8
Donggaem <ehdrua465111@naver.com>
2023-07-29 22:47:32 +0900

    중복확인 버튼이 들어간 커스텀 텍스트필드 구현

--- TODO-Refactor/CoustomUI/CoustomTextField.swift
+++ TODO-Refactor/CoustomUI/CoustomTextField.swift
@@ -17,6 +17,7 @@ class CoustomTextField: UIView {
         lazy var view = UIView().then {
             $0.backgroundColor = TODOColor.white_FF
         }
+        
         lazy var textField = UITextField().then {
             $0.font = TODOFont.roboto_Re(20)
             $0.textColor = TODOColor.black_00
@@ -39,7 +40,7 @@ class CoustomTextField: UIView {
         
         textField.snp.makeConstraints { make in
             make.width.equalTo(326)
-            make.height.equalTo(27)
+            make.height.equalTo(19)
             make.top.equalTo(view.snp.top).inset(0)
             make.leading.equalTo(view.snp.leading).inset(0)
             make.trailing.equalTo(view.snp.trailing).inset(0)
@@ -49,7 +50,70 @@ class CoustomTextField: UIView {
         line.snp.makeConstraints { make in
             make.width.equalTo(326)
             make.height.equalTo(1)
-            make.top.equalTo(textField.snp.bottom).offset(1)
+            make.top.equalTo(textField.snp.bottom).offset(8)
+            make.bottom.equalTo(view.snp.bottom).inset(0)
+            make.leading.equalTo(view.snp.leading).inset(0)
+            make.trailing.equalTo(view.snp.trailing).inset(0)
+        }
+        
+        return view
+    }
+    
+    //중복확인이 있는 textfield
+    static func coustomTextField_checkbtn(_ placeHolder: String) -> UIView {
+        lazy var view = UIView().then {
+            $0.backgroundColor = TODOColor.white_FF
+        }
+        
+        lazy var textField = UITextField().then {
+            $0.font = TODOFont.roboto_Re(20)
+            $0.textColor = TODOColor.black_00
+            $0.placeholder = placeHolder
+            $0.setPlaceholderColor(TODOColor.gray_83)
+            
+        }
+        
+        lazy var line = UIView().then {
+            $0.backgroundColor = TODOColor.gray_DD
+        }
+        
+        lazy var checkBtn = UIButton(type: .custom).then {
+            $0.backgroundColor = TODOColor.blue_3B
+            $0.setTitle("중복확인", for: .normal)
+            $0.titleLabel?.font = TODOFont.roboto_Re(12)
+            $0.setTitleColor(TODOColor.white_FF, for: .normal)
+            $0.layer.cornerRadius = 5
+            
+        }
+        
+        view.addSubview(textField)
+        view.addSubview(checkBtn)
+        view.addSubview(line)
+        
+        view.snp.makeConstraints { make in
+            make.width.equalTo(326)
+            make.height.equalTo(29)
+        }
+        
+        textField.snp.makeConstraints { make in
+            make.width.equalTo(246)
+            make.height.equalTo(19)
+            make.top.equalTo(view.snp.top).inset(0)
+            make.leading.equalTo(view.snp.leading).inset(0)
+        }
+        
+        checkBtn.snp.makeConstraints { make in
+            make.width.equalTo(79)
+            make.height.equalTo(19)
+            make.centerX.equalTo(textField)
+            make.leading.equalTo(textField.snp.trailing).offset(-1)
+            make.trailing.equalTo(view.snp.trailing).inset(0)
+        }
+        
+        line.snp.makeConstraints { make in
+            make.width.equalTo(326)
+            make.height.equalTo(1)
+            make.top.equalTo(textField.snp.bottom).offset(8)
             make.bottom.equalTo(view.snp.bottom).inset(0)
             make.leading.equalTo(view.snp.leading).inset(0)
             make.trailing.equalTo(view.snp.trailing).inset(0)
---
+++ TODO-Refactor/Login/SignUPViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SignUPViewController.swift
+//  TODO-Refactor
+//
+//  Created by 김동겸 on 2023/07/29.
+//
+
+import UIKit
+
+class SignUPViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = TODOColor.white_FF
+
+    }
+
+}
+
+//UI 미리보기
+#if DEBUG
+import SwiftUI
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    
+    func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+    }
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        SignUPViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+    
+    func toPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
+#endif
