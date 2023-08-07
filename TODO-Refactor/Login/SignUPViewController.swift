ebc2233962e8d355028b83d33c1ed76df1b9b5b1
Donggaem <ehdrua465111@naver.com>
2023-08-07 18:18:57 +0900

    회원가입 화면 UI구성

--- TODO-Refactor/Login/SignUPViewController.swift
+++ TODO-Refactor/Login/SignUPViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class SignUPViewController: UIViewController {
+    
+    private var label_TODO = UILabel().then {
+        $0.textAlignment = .center
+        $0.textColor = TODOColor.black_00
+        $0.font = TODOFont.roboto_Me(60)
+        $0.text = "TODO"
+    }
+    
+    private var idTextField = CoustomTextField.coustomTextField_checkbtn("아이디를 입력하세요")
+    private var nickNameTextField = CoustomTextField.coustomTextField_checkbtn("닉네임을 입력하세요")
+    private var pwTextField = CoustomTextField.coustomTextField("비밀번호를 입력하세요")
+    private var pwCheckTextField = CoustomTextField.coustomTextField("비밀번호를 확인해주세요")
+    
+    private var btn_Signup = CoustomButton.coustomBtn_main("회원가입")
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = TODOColor.white_FF
+        
+        setUI()
 
+    }
+    
+    private func setUI() {
+        
+        self.view.addSubview(label_TODO)
+        
+        self.view.addSubview(idTextField)
+        self.view.addSubview(nickNameTextField)
+        self.view.addSubview(pwTextField)
+        self.view.addSubview(pwCheckTextField)
+        
+        self.view.addSubview(btn_Signup)
+        
+        label_TODO.snp.makeConstraints { make in
+            make.top.equalTo(self.view).inset(150)
+            make.centerX.equalToSuperview()
+        }
+        
+        idTextField.snp.makeConstraints { make in
+            make.top.equalTo(label_TODO.snp.bottom).offset(203)
+            make.leading.equalToSuperview().inset(32)
+            make.trailing.equalToSuperview().inset(32)
+        }
+        
+        nickNameTextField.snp.makeConstraints { make in
+            make.top.equalTo(idTextField.snp.bottom).offset(32)
+            make.leading.equalToSuperview().inset(32)
+            make.trailing.equalToSuperview().inset(32)
+        }
+        
+        pwTextField.snp.makeConstraints { make in
+            make.top.equalTo(nickNameTextField.snp.bottom).offset(32)
+            make.leading.equalToSuperview().inset(32)
+            make.trailing.equalToSuperview().inset(32)
+        }
+        
+        pwCheckTextField.snp.makeConstraints { make in
+            make.top.equalTo(pwTextField.snp.bottom).offset(32)
+            make.leading.equalToSuperview().inset(32)
+            make.trailing.equalToSuperview().inset(32)
+        }
+        
+        
+        btn_Signup.snp.makeConstraints { make in
+            make.width.equalTo(326)
+            make.height.equalTo(48)
+            make.top.equalTo(pwCheckTextField.snp.bottom).offset(94)
+            make.bottom.equalToSuperview().inset(75)
+            make.leading.equalToSuperview().inset(32)
+            make.trailing.equalToSuperview().inset(32)
+        }
     }
 
 }
